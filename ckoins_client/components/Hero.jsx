073edd8733eb4fdb0f5a3360a4cd10140521fba0
Steerpike <imthekoul@gmail.com>
2022-10-12 02:24:56 +0300

    Hero && Trending Sections

--- ckoins_client/components/Hero.jsx
+++ ckoins_client/components/Hero.jsx
@@ -5,13 +5,14 @@ const AlignBg = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
-  background-color: #111827;
+  background-color: ${props => props.theme.main};
 `;
 
 const HeroContainer = styled.div`
   height: 100vh;
-  width: ${props => (props.inView ? '100%' : '80%')};
+  width: ${props => (props.inView ? '100%' : '79.7%')};
   border: ${props => (props.inView ? '0px' : '2px')} solid white;
+  border-bottom: none;
   transition: all 500ms ease;
   background-position: center;
   background-image: url('/hero.jpg');
